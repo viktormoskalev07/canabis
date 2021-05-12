a4b64fc8d97a54e1a526754442b35e7b0a737ce1
unknown <viktormoskalev07@gmail.com>
2021-05-12 12:34:47 +0300

    add swiper

--- src/js/main.js
+++ src/js/main.js
@@ -148,4 +148,32 @@ drop.addEventListener('click', function(){
  try{
   dropdown()
  } catch (e) {console.log(e)}
+//dropdown
 
+
+// categories swiper 
+const swiper = new Swiper('.swiper-container', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    765: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1080: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
+})
+
+// categories swiper 
