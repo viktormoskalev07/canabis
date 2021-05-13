8fe5d799afc47a024eeb591998a1a70e2464fcd5
Shangri <astasenko@smart-ui.pro>
2021-05-13 13:57:51 +0300

    created accordion

---
+++ src/js/accordion.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function (){
+    const acc = document.getElementsByClassName("accordion");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+});
