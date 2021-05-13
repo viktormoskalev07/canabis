82c38c76d692c9b12f9d16bdc8e3029cc55b169a
Shangri <astasenko@smart-ui.pro>
2021-05-13 14:29:42 +0300

    created accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,16 +1,21 @@
-window.addEventListener('load', function (){
-    const acc = document.getElementsByClassName("accordion");
+window.addEventListener('load', function () {
+
+    const accordion = document.getElementsByClassName("accordion");
     let i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            const panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
+    if (!accordion) {
+        return null;
+    } else {
+        for (i = 0; i < accordion.length; i++) {
+            accordion[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                const panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            });
+        }
     }
 });
