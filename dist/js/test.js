967c6bc76b300e6cfd1418ba984331090c9d8782
viktormoskalev07 <neonchilkk@gmail.com>
2021-05-16 19:33:46 +0300

    remove roadmap optimize bg

--- dist/js/test.js
+++
@@ -1,1 +0,0 @@
-const swiper=new Swiper(".swiper-categories",{slidesPerView:1,spaceBetween:10,breakpoints:{320:{slidesPerView:1.5,spaceBetween:20},576:{slidesPerView:2,spaceBetween:30},992:{slidesPerView:6,spaceBetween:40}}});
